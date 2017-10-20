88b0be1d8c4e54da2aab4fd591359970bfdac31e
yoginski <yoginski@gmail.com>
2017-10-20 22:10:38 +0600

    Передумал насчет вебхуков

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python bot.py
+python bot.py
--- bot.py
+++ bot.py
@@ -1,4 +1,9 @@
+import logging
 from telegram.ext import Updater, CommandHandler
+
+logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO)
 
 def start(bot, update):
     update.message.reply_text('Hello World!')
@@ -7,7 +12,7 @@ def hello(bot, update):
     update.message.reply_text(
         'Hello {}'.format(update.message.from_user.first_name))
 
-updater = Updater('YOUR TOKEN HERE')
+updater = Updater('477027524:AAHjzfguxYlhM1KZDUUd8vW3oihd4MsKnx0')
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
